95f01b0735307c84063d86f06bdebe053e36152d
origotdaskill <139981840+origotdaskill@users.noreply.github.com>
2024-10-10 15:50:35 +0100

    add admin Header :lipstick:

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import AdminSideBar from "./sidebar";
+import AdminSideBar from "./sidebar.jsx";
 import AdminHeader from "./header";
 import { useState } from "react";
 
@@ -11,7 +11,7 @@ function AdminLayout() {
       {/* admin sidebar */}
       <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col">
-        {/* Admin header */}
+        {/* admin header */}
         <AdminHeader setOpen={setOpenSidebar} />
         <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
           <Outlet />
