11c0d38881cc7809f7e65bcb987dcd9c5314ef95
origotdaskills <mr.walid.ori@gmail.com>
2024-10-14 23:35:30 +0100

    update paypal :closed_lock_with_key:

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "EK5veDQ87Yx8DV9qoHvSthShuQzJTewAmTLSAUp1WvDHy5hTb1GdRKyCkfBOWgIft_1GrR3ELpZPgbam",
-  client_secret: "EK5veDQ87Yx8DV9qoHvSthShuQzJTewAmTLSAUp1WvDHy5hTb1GdRKyCkfBOWgIft_1GrR3ELpZPgbam",
+  client_id: "AUY6EMgWMPSeHKGCd7W3VbSqoYfReNnUNelUSnaMnGj8YGLBNjdKgIbN7jnIWWK0b-KVEQbPufQh9oQV",
+  client_secret: "EFuKhbO7H65wegVNoHv2UD42e0hwKN7BVTGOFoVFb0H4GSLL5-2mrAHPWKhDPGpqR-oAiovsdfI_jeW-",
 });
 
 module.exports = paypal;
