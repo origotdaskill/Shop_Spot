108a57cd7a6c7869c60b9db65bc77403d76da7b9
origotdaskill <139981840+origotdaskill@users.noreply.github.com>
2024-10-14 14:04:41 +0100

    update client  deployment URl :lipstick:

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -13,7 +13,7 @@ export const registerUser = createAsyncThunk(
   "/auth/register",
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/register",
+      "https://shop-spot-backend.onrender.com/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -27,7 +27,7 @@ export const loginUser = createAsyncThunk(
   "/auth/login",
   async (formData) => {
     const response = await axios.post(
-      "http://localhost:5000/api/auth/login",
+      "https://shop-spot-backend.onrender.com/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -39,7 +39,7 @@ export const loginUser = createAsyncThunk(
 
 export const logoutUser = createAsyncThunk("/auth/logout", async () => {
   const response = await axios.post(
-    "http://localhost:5000/api/auth/logout",
+    "https://shop-spot-backend.onrender.com/api/auth/logout",
     {},
     {
       withCredentials: true,
@@ -63,7 +63,7 @@ export const checkAuth = createAsyncThunk("/auth/checkauth", async () => {
 
   // If no local storage data, check with API
   const response = await axios.get(
-    "http://localhost:5000/api/auth/check-auth",
+    "https://shop-spot-backend.onrender.com/api/auth/check-auth",
     {
       withCredentials: true,
       headers: {
